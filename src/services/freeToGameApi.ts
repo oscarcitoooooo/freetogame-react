5ad7b4d9c2ea49d68353ca1fed23962bb93bf485
Yoseth <lyoreda@gmail.com>
2026-09-18 09:32:36 -0500

    refactor: tipar respuesta del servicio de juegos

--- src/services/freeToGameApi.ts
+++ src/services/freeToGameApi.ts
@@ -1,6 +1,7 @@
+import type { Game } from '../types/Game'
 const BASE_URL = 'https://www.freetogame.com/api'
 
-export const getGames = async () => {
+export const getGames = async (): Promise<Game[]> => {
   const response = await fetch(`${BASE_URL}/games`)
   return response.json()
 }
