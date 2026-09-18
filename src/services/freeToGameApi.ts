97eee04e015ec820d10369d8f636fa33aa615e0e
Yoseth <lyoreda@gmail.com>
2026-09-18 09:49:58 -0500

    feat: agregar manejo de errores al servicio de juegos

--- src/services/freeToGameApi.ts
+++ src/services/freeToGameApi.ts
@@ -1,7 +1,13 @@
 import type { Game } from '../types/Game'
+ 
 const BASE_URL = 'https://www.freetogame.com/api'
-
+ 
 export const getGames = async (): Promise<Game[]> => {
   const response = await fetch(`${BASE_URL}/games`)
+ 
+  if (!response.ok) {
+    throw new Error('Error al obtener los videojuegos')
+  }
+ 
   return response.json()
 }
